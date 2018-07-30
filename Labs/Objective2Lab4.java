eea1cb86fef07a8b0285b888c24cd6ff80cb91e4
Kyle Paladini <krpaladini@gmail.com>
2018-07-30 13:41:15 -0600

    Obj2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,13 @@
+public class Objective2Lab4 {
+    public static void main( String [] args ) {
+
+      double num1, num2, num3, average;
+      num1 = 5;
+      num2 = 20;
+      num3 = 43;
+      average = (num1 + num2 + num3)/3;
+      System.out.println( "The average of " + num1 + ", " + num2 + ", " + num3 + " is: ");
+      System.out.println( average );
+
+    }
+}
