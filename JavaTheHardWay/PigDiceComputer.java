151f0c5d9e1dbb148a51b644d61d10accc04aae3
papadini <krpaladini@gmail.com>
2018-08-14 13:38:26 -0600

    PigDiceComputer

---
+++ JavaTheHardWay/PigDiceComputer.java
@@ -0,0 +1,24 @@
+public class PigDiceComputer {
+    public static void main(String [] args) {
+      int roll, total;
+      total = 0;
+
+      do {
+        roll = 1 + (int)(Math.random()*6);
+        System.out.println("Computer rolled a " + roll + ".");
+        total =+ roll;
+
+        if (roll == 1 ) {
+          System.out.println( "\tThat ends its turn.");
+        }
+        else {
+          System.out.print( "\tComputer has " + total );
+          System.out.print( " points so far this round.\n");
+          if ( total < 20 ) {
+            System.out.println("\tComputer will roll again." );
+          }
+        }
+      } while ( roll != 1 && total < 20 );
+      System.out.println("Computer ends round with " + total + " points");
+    }
+}
