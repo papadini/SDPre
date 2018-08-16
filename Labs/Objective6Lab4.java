48c93ff1a0d28ee1675bdd39d55dd5645363841b
papadini <krpaladini@gmail.com>
2018-08-16 10:25:00 -0600

    Obj6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,13 @@
+public class Objective6Lab4 {
+  public static void main( String [] args ) {
+    int count = 0, sum = 0;
+
+    while ( count < 20 ) {
+      count ++;
+      sum = sum + count;
+    }
+
+    System.out.println( sum );
+
+  }
+}
