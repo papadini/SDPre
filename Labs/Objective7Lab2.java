8fbf3b331dd3bcbfd42f19da6a2738af0b9daf7f
papadini <krpaladini@gmail.com>
2018-08-22 21:33:52 -0600

    Obj7Lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,10 @@
+public class Objective7Lab2 {
+  public static void main( String [] args ) {
+    System.out.println("in main");
+    printMessage();
+    System.out.println("in main");
+  }
+  public static void printMessage () {
+    System.out.println("in method");
+  }
+}
